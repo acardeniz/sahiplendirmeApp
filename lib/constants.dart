41b4516f5e34b6e47faa92622b900854e4f24fcb
acardeniz <denizacar19731973@hotmail.com>
2025-10-11 23:36:45 +0300

    colors changed

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,27 +1,10 @@
-// lib/constants.dart
-
 import 'package:flutter/material.dart';
 
-// AYDINLIK TEMA RENKLERİ (Güven ve Doğa Tonları)
-const Color primaryColor = Color(0xFF41A67E); // Sakin Yeşil (ANA Vurgu)
-const Color secondaryColor = Color(
-  0xFF05339C,
-); // Derin Gece Mavisi (AppBar, Güven)
-const Color lightBackgroundColor = Color.fromARGB(
-  255,
-  228,
-  205,
-  125,
-); // Hafif Açık Gri (Scaffold BG)
-const Color lightGreyColor = Color(
-  0xFF1055C9,
-); // Canlı Mavi (Detaylar, Sınırlar)
+const Color primaryColor = Color(0xFFE62727);
+const Color secondaryColor = Color(0xFF1E93AB);
+const Color lightBackgroundColor = Color(0xFFF3F2EC);
+const Color lightGreyColor = Color(0xFFDCDCDC);
 
-// KARANLIK TEMA RENKLERİ
-const Color darkPrimaryColor = Color(
-  0xFFE5C95F,
-); // Altın Sarısı (Karanlık Vurgu)
-const Color darkSecondaryColor = Color(
-  0xFF05339C,
-); // Derin Gece Mavisi (Karanlık App Bar)
-const Color darkBackgroundColor = Color(0xFF1C1C1C); // Koyu Gri (Karanlık BG)
+const Color darkPrimaryColor = Color(0xFFE62727);
+const Color darkSecondaryColor = Color(0xFF1E93AB);
+const Color darkBackgroundColor = Color(0xFF121212);
